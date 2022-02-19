b0dd2c534ae27b701fa07795664cbac0127cbf43
Michal Milewski <michal.milewski98@gmail.com>
2022-02-19 04:19:41 +0100

    Deploy contract using metamask

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 
 describe("Hello world", function() {
-    it("should say Hello, World", async function() {
+    it("should say Hello, World", async () => {
         const HelloWorld = await ethers.getContractFactory("HelloWorld");
         const hello = await HelloWorld.deploy();
         await hello.deployed();
